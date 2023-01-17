23aec7fddfe8210eb49311d726999d0271cfed03
vasu-ae <vasu.vadivel@aenetworks.com>
2023-01-17 18:11:24 +0530

    test

--- security-group/main.tf
+++ security-group/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "security_group" {
-    count = var.create_security_group || var.create_eks_cluster_security_group || var.create_eks_worker_security_group ? 1 : 0
+    count = local.control && var.create_security_group || var.create_eks_cluster_security_group || var.create_eks_worker_security_group ? 1 : 0
   #name = var.security_group_name #Required
   name =  var.security_group_name == null && var.create_eks_cluster_security_group == false && var.create_eks_worker_security_group == false ? upper(join("-",[(var.environment == "DRE" ? "AZO" : "AZV"), join("",["SG",var.server_type]), join("",["${upper(var.environment)=="DRE" || upper(var.environment)=="DBG" ? substr(var.environment,1,1) : substr(var.environment,0,1) }","${var.application_id}"]) ])) : var.security_group_name == null && var.create_eks_cluster_security_group ? upper(join("-",[(var.environment == "DRE" ? "AZO" : "AZV"),"EKS",join("",["CLU","SG"]),join("",["${upper(var.environment)=="DRE" || upper(var.environment)=="DBG" ? substr(var.environment,1,1) : substr(var.environment,0,1) }",var.LOB]) ])) : var.security_group_name == null && var.create_eks_worker_security_group ? upper(join("-",[(var.environment == "DRE" ? "AZO" : "AZV"),"EKS",join("",["SG" ,var.server_type ]),join("",["${upper(var.environment)=="DRE" || upper(var.environment)=="DBG" ? substr(var.environment,1,1) : substr(var.environment,0,1) }",var.LOB]) ])) :  var.security_group_name
   description = var.description #optional
@@ -27,3 +27,6 @@ resource "aws_security_group_rule" "security_group_rules" {
   source_security_group_id =  try(var.security_group_rules[count.index].source_security_group_id,try(var.security_group_rules[count.index].source_security_group, false) ? aws_security_group.security_group[0].id : null)
 }
 
+locals {
+  control = var.application_id == null && var.environment == null ? false : true
+}
