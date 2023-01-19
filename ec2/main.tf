ea5347f3129887f5e581c7654bbde5ee544d76f0
vasu-ae <vasu.vadivel@aenetworks.com>
2023-01-19 19:58:54 +0530

    volume tags added

--- ec2/main.tf
+++ ec2/main.tf
@@ -54,7 +54,7 @@ resource "aws_instance" "ec2_instance" {
 
   volume_tags = merge(
     {
-     "Name"        = upper(join("-",[(var.environment == "DRE" ? "AZO" : "AZV"), var.standard_name["server_type"],join("",["${upper(var.environment)=="DRE" || upper(var.environment)=="DBG" ? substr(var.environment,1,1) : substr(var.environment,0,1) }","${var.application_id}"]) ]))
+     "Name"        = upper(join("-",[(var.environment == "DRE" ? "AZO" : "AZV"), upper(var.standard_name["server_type"]),join("",["${upper(var.environment)=="DRE" || upper(var.environment)=="DBG" ? substr(var.environment,1,1) : substr(var.environment,0,1) }","${var.application_id}"]) ]))
       "Environment" = var.environment
       "Application ID" = var.application_id
     },var.default_tags)  
